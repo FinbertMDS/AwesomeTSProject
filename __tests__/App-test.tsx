11a3a4162662013e10f804da0d8bc67e6c9c064c
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2021-06-15 19:53:18 +0900

    update

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -12,8 +12,3 @@ import App from '../App';
 it('renders correctly', () => {
   renderer.create(<App />);
 });
-
-it('random error', () => {
-  const random_boolean = Math.random() < 0.3;
-  expect(random_boolean).toBe(true)
-});
