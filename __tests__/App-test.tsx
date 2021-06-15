0a6cf4ff2d13247830dd82b4042e6bda0b0e16ec
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2021-06-15 19:43:57 +0900

    Restarts the scheduled run when it failed

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,18 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from '../App';
-
+import 'react-native';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import App from '../App';
+
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('random error', () => {
+  const random_boolean = Math.random() < 0.3;
+  expect(random_boolean).toBe(true)
 });
